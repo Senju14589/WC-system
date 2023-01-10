9f2f0737615301540d6a82374a1248fedc08e651
นายพุฒิพงศ์  สักแสน <59078952+Senju14589@users.noreply.github.com>
2023-01-10 10:55:27 +0700

    update

--- WC_system/resources/views/member/index.blade.php
+++ WC_system/resources/views/member/index.blade.php
@@ -33,45 +33,46 @@
 
 <!-- Javascript นี้เป็นวิธีการดึงที่อยู่ของผู้ใช้จากGPSของเครื่องผู้ใช้ นำมาคำนวนหาพื้นที่ในการเช็คเข้าใช้งาน ในกรณีที่ผู้ใช้อยู่นพื้นที่ที่กำหนดก็สามารถเข้าไปหน้าถัดไปได้แต่ถ้าไม่ก็แจ้งErrorกลับไป-->
 <script>
-    var x = document.getElementById("demo");
-
     function getLocation() {
+        console.log('getLocation called');
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition);
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var lat1 = position.coords.latitude;
+                var lon1 = position.coords.longitude;
+                var lat2 = 16.45576013923571;
+                var lon2 = 102.81952709756617;
+                var distance = calculateDistance(lat1, lon1, lat2, lon2);
+
+                function calculateDistance(lat1, lon1, lat2, lon2) {
+                    var R = 6371; // Radius of the earth in km
+                    function deg2rad(deg) {
+                        return deg * (Math.PI / 180)
+                    }
+                    var dLat = deg2rad(lat2 - lat1); // deg2rad below
+                    var dLon = deg2rad(lon2 - lon1);
+                    var a =
+                        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+                        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+                    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+                    var d = R * c; // Distance in km
+                    return d;
+                }
+                if (distance <= 500) {
+                    // The user is within 500 meters of the designated location
+                    // Do something (such as redirecting the user to a different page)
+                    window.location.href = '/checkin';
+                } else {
+                    // The user is not within 500 meters of the designated location
+                    // Do something else (such as displaying an error message)
+                    x.innerHTML = "You are not in the allowed location.";
+                }
+            });
         } else {
+            console.log('navigator.geolocation not supported');
             x.innerHTML = "Geolocation is not supported by this browser.";
         }
     }
 
-    function calculateDistance(lat1, lon1, lat2, lon2) {
-        var R = 6371; // Radius of the earth in km
-        var dLat = deg2rad(lat2 - lat1); // deg2rad below
-        var dLon = deg2rad(lon2 - lon1);
-        var a =
-            Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-            Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-            Math.sin(dLon / 2) * Math.sin(dLon / 2);
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        var d = R * c; // Distance in km
-        return d;
-    }
-
-
-    function deg2rad(deg) {
-        return deg * (Math.PI / 180)
-    }
-
-    function showPosition(position) {
-        // Calculate distance between user's location and designated area
-        var distance = calculateDistance(position.coords.latitude, position.coords.longitude, 16.45571346138386,
-            102.81952262321487);
-
-        if (distance <= 500) {
-            // Allow access to the login page
-            window.location.href = '/checkin/normal';
-        } else {
-            // Display an error message
-            x.innerHTML = "You are not in the allowed location.";
-        }
-    }
+    var x = document.getElementById("demo");
 </script>
